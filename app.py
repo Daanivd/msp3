610ac528b2419c7580d991ea31a695900d5d9cf5
Daniel van Duinkerken <daanivd@hotmail.com>
2019-04-24 10:29:25 +0000

    ingredients and allergens now showing

--- app.py
+++ app.py
@@ -17,7 +17,10 @@ def recipes():
 @app.route('/recipes/<recipe_id>')
 def view_recipe(recipe_id):
     the_recipe =  mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
-    return render_template('recipe.html', recipe=the_recipe,
+    
+    the_ingredients = mongo.db.recipes.distinct('ingredients')
+    the_allergens =  mongo.db.recipes.distinct('allergens')
+    return render_template('recipe.html', recipe=the_recipe, ingredients=the_ingredients, allergens=the_allergens
     )
  
 @app.route('/create')

0c7c621f8e8223d031911a8cb262fe5e06f264f8
Daniel van Duinkerken <daanivd@hotmail.com>
2019-05-09 13:43:06 +0000

    added styling and images to templates

--- static/js/functions.js
+++ static/js/functions.js
@@ -8,9 +8,21 @@ $(document).ready(function() {
         var NewIngredient = ""
         $('select').material_select();
     });
-    
-   $("i").click(function() {
-  console.log($(this).parent()[0].id);
-});
+
+    $('i').click(function() {
+        var id = ($(this).parent()[0].id);
+        console.log(id)
+        $('input[value=' + id + ']').removeClass('active').addClass('selected');
+
+
+    });
+
+   
+        // Get height for textarea div in edit/add recipe routes
+        height = $('.height').outerHeight();
+        console.log(height);
+        $(".directions").outerHeight(height);
+
+
 
 });

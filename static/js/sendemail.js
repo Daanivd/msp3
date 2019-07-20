956391eaa885cdefcef3463edf2f653b3a1e374b
Daniel van Duinkerken <daanivd@hotmail.com>
2019-07-20 15:42:52 +0000

    added contact functionality with emailjs

---
+++ static/js/sendemail.js
@@ -0,0 +1,20 @@
+console.log('hello')
+function sendMail(contactForm) {
+    emailjs.send("default_service", "template_kM7czE4k", {
+            from_name: contactForm.name.value,
+            from_email: contactForm.email.value,
+            subject: contactForm.subject.value,
+            message: contactForm.message.value
+        })
+        .then(
+            function(response) {
+                console.log("SUCCESS", response);
+
+            },
+            function(error) {
+                console.log("FAILED", error);
+            }
+        )
+    return false // To block from loading a new page
+}
+

eaf516cc815348b0eef489bc20778c59f6791b94
Daniel van Duinkerken <daanivd@hotmail.com>
2019-07-21 18:18:11 +0000

    removed console.log(hello) from sendemail.js

--- static/js/sendemail.js
+++ static/js/sendemail.js
@@ -1,20 +1,19 @@
-console.log('hello')
 function sendMail(contactForm) {
-    emailjs.send("default_service", "template_kM7czE4k", {
-            from_name: contactForm.name.value,
-            from_email: contactForm.email.value,
-            subject: contactForm.subject.value,
-            message: contactForm.message.value
+    emailjs.send('default_service', 'template_kM7czE4k', {
+            'from_name': contactForm.name.value,
+            'from_email': contactForm.email.value,
+            'subject': contactForm.subject.value,
+            'message': contactForm.message.value
         })
         .then(
             function(response) {
-                console.log("SUCCESS", response);
-
+                console.log('SUCCESS', response);
             },
+            
             function(error) {
-                console.log("FAILED", error);
+                console.log('FAILED', error);
             }
-        )
-    return false // To block from loading a new page
+        );
+    return false; // To block from loading a new page
 }
 

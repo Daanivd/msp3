c52cfe7c1ff853ba66221e7061afcf359e0e6a1d
Daniel van Duinkerken <daanivd@hotmail.com>
2019-07-22 08:56:35 +0000

    fixed column issue for adding ingredient/step in functions.js

--- static/js/functions.js
+++ static/js/functions.js
@@ -1,22 +1,16 @@
 $(document).ready(function() {
-
+    
     $('.addIngredient').click(function() {
-
-        var NewIngredient = '<div class="input-field col s12"><input id="ingredients" type="text" name="ingredients"><label for="ingredients">ingredient</label></div>'
-
+        var NewIngredient = '<div class="input-field"><input id="ingredients" type="text" name="ingredients"><label for="ingredients">ingredient</label></div>'
         $(".ingredients").append(NewIngredient);
         var NewIngredient = ""
         $('select').material_select();
     });
     
-    
-    n=1;
     $('.addStep').click(function() {
         var NewStep = '<li><div class="input-field"><textarea  name="directions" class="materialize-textarea"></textarea><label for="directions"></label></div></li>'
         $(".directions").append(NewStep);
         var NewStep = ""
         $('select').material_select();
     });
-
- 
 });
